07f5156c385a498a94b85fe3dae2c49b653bacc1
Teaaa <tealico@hotmail.com>
2022-04-06 02:04:14 +0200

    Failed to load application context

--- src/test/java/com/task/integration/UserIntegrationTests.java
+++ src/test/java/com/task/integration/UserIntegrationTests.java
@@ -1,20 +1,25 @@
 package com.task.integration;
 
+
 import com.task.dto.UserDto;
-import com.task.entity.UserEntity;
 import com.task.service.UserService;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@ContextConfiguration(locations = "classpath:WEB-INF/application-context.xml")
 public class UserIntegrationTests {
 
     @Autowired
@@ -30,6 +35,12 @@ public class UserIntegrationTests {
 
     @Before
     public void setUserService(){
-        given(this.userService.getById(1)).willReturn(new UserDto("Ana","annabelle","Software Engineer","ana@gmail.com"));
+        given(this.userService.getById(1))
+                .willReturn(new UserDto(
+                        "Ana",
+                        "annabelle",
+                        "Software Engineer",
+                        "ana@gmail.com"
+                ));
     }
 }
